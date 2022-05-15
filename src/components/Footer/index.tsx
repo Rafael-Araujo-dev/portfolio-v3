f34c5f3f944a68fd1f020210befe1f5569ddd097
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-05-15 01:22:20 -0300

    feat: cria componente footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,11 +5,36 @@ import { useThemeState } from "@context/theme";
 
 import { Container } from "./styles";
 
+import data from "./data.json";
+
 const Footer: NextPage = () => {
   const [language, setLanguage] = useLanguageState();
   const [theme, setTheme] = useThemeState();
 
-  return <Container theme={theme}>Footer</Container>;
+  const dataObj: {
+    [key: string]: {
+      footer: Array<{ name: string; link: string }>;
+    };
+  } = data;
+
+  return (
+    <Container theme={theme}>
+      <div>
+        <span>Rafael Ribeiro de Araujo - 2022</span>
+        <ul role="footer-nav">
+          {dataObj[language].footer.map(
+            (item: { name: string; link: string }, index: number) => {
+              return (
+                <li key={index}>
+                  <a href={item.link}>{item.name}</a>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,7 +3,7 @@ import colors from "@styles/colors.json";
 import typography from "@styles/typography.json";
 
 export const Container = styled.footer<{theme?: string}>`
-    padding: 0 5%;
+    padding: 40px 5%;
     background-color: ${props =>
         props.theme == "Light" && colors.neutral100
         ||
@@ -11,4 +11,54 @@ export const Container = styled.footer<{theme?: string}>`
     };
     transition-duration: 250ms;
     transition-property: background;
+
+    div {
+        max-width: 1240px;
+        margin: auto;
+        display: flex;
+        flex-direction: column-reverse;
+        align-items: center;
+        gap: 20px;
+        @media (min-width: 768px) {
+            flex-direction: row;
+            justify-content: space-between;
+        }
+    }
+
+    span {
+        font-family: "DM Sans", sans-serif;
+        font-size: ${typography.title5.fontSize};
+        font-weight: ${typography.title5.fontWeight.medium};
+        line-height: ${typography.title5.lineHeight};
+        color: ${props =>
+            props.theme == "Light" && colors.secondary800
+            ||
+            props.theme == "Dark" && colors.neutral100
+        };
+        transition-property: color;
+        transition-duration: 250ms;
+    }
+
+    ul, ul a {
+        font-family: "DM Sans", sans-serif;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        column-gap: 20px;
+        font-size: ${typography.title5.fontSize};
+        font-weight: ${typography.title5.fontWeight.medium};
+        line-height: ${typography.title5.lineHeight};
+        color: ${props =>
+            props.theme == "Light" && colors.neutral900
+            ||
+            props.theme == "Dark" && colors.neutral100
+        };
+        transition-property: color;
+        transition-duration: 250ms;
+        cursor: pointer;
+        
+    }
+    ul li:hover, ul a:hover {
+        text-decoration: underline;
+    }
 `
