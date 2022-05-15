84d98be03a79b09a3fb06d909b7e9dae6065f53a
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2022-05-15 12:25:10 -0300

    feat: adiciona aos library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,18 @@ import GlobalStyled from "@styles/GlobalStyled";
 import { LanguageProvider } from "@context/language";
 import { ThemeProvider } from "@context/theme";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      offset: 50,
+      once: true,
+    });
+  }, []);
+
   return (
     <>
       <ThemeProvider>
